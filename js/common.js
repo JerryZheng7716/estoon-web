6cf502cd6e61fa2af034b74d7f36a795526eef1e
JerryZheng7716 <740117716@qq.com>
2019-04-12 13:53:53 +0800

    完成微信小程序介绍

--- js/common.js
+++ js/common.js
@@ -1,10 +1,22 @@
 $(function () {
+
+    var isMouseOverDropDowm = false;
+    $(".dropdown").click(function () {//防止手机端，点一次展不开菜单栏
+        if (isMouseOverDropDowm) {
+            $(this).removeClass("open");
+            $(this).children('.dropdown-toggle').attr('aria-expanded',false);
+            isMouseOverDropDowm=false;
+        }
+    });
+
     $(".dropdown").mouseover(function () {
+        isMouseOverDropDowm=true;
         $(this).addClass("open");
+        $(this).children('.dropdown-toggle').attr('aria-expanded',true);
     });
 
     $(".dropdown").mouseleave(function(){
         $(this).removeClass("open");
+        $(this).children('.dropdown-toggle').attr('aria-expanded',false);
     })
-
 });
